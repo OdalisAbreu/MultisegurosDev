f58a5f85ba118bb1712654ade19ad54fbb513e4e
Dariel Abreu <odalisdabrue@gmail.com>
2022-01-30 17:36:08 -0400

    Add  Tipo en modificar Modelo 1.1.3.9

--- controller/VehiculoController.php
+++ controller/VehiculoController.php
@@ -8,7 +8,7 @@ class vehiculoController{
         $r2 = mysql_query("SELECT * from seguro_modelos WHERE ID ='".$id."'");
         $row = mysql_fetch_array($r2);
 
-        return $id;
+        return $row;
     }
 
 }
