3f8bae7a87ca14df175425ef0ce01157a1585c02
Dariel Abreu <odalisdabrue@gmail.com>
2022-01-30 19:38:38 -0400

    V

--- controller/VehiculoController.php
+++ controller/VehiculoController.php
@@ -4,22 +4,19 @@ class vehiculoController{
 
     // Optiene los tipos de Vehiculos 
     function getTypes(){
-        $consult = mysql_query(
-            "SELECT id, nombre, veh_tipo from seguro_tarifas order by nombre ASC"
-        );
-         return  $consult;
+        $consult = mysql_query("SELECT id, nombre, veh_tipo from seguro_tarifas order by nombre ASC"        );
+        $row = mysql_fetch_array($consult);
+        return  $row;
     }
     // Optiene un tipo de Vehiculos po ID 
         function getType($id){
-            $consult = mysql_query(
-                "SELECT id, nombre, veh_tipo FROM seguro_tarifas WHERE veh_tipo = $id"
-            );
-             return  $consult;
+            $consult = mysql_query("SELECT id, nombre, veh_tipo FROM seguro_tarifas WHERE veh_tipo = $id"            );
+            $row = mysql_fetch_array($consult);
+             return  $row;
         }
 
     //Optiene los modelos de los vehiculos por su ID
     function getModelo($id){
-        Conectarse();
         $r2 = mysql_query("SELECT * from seguro_modelos WHERE ID ='".$id."'");
         $row = mysql_fetch_array($r2);
 
