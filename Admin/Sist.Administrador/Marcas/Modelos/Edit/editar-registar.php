57f3be4b82139c02d2f47e3b007adc6d0ae5b88d
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-07 22:21:53 -0400

    V. 1.1.6.7

--- Admin/Sist.Administrador/Marcas/Modelos/Edit/editar-registar.php
+++ Admin/Sist.Administrador/Marcas/Modelos/Edit/editar-registar.php
@@ -73,8 +73,8 @@
                                                      $modelId = $_GET['id'];
                                                      $rescat = mysql_query("SELECT id, nombre, veh_tipo from seguro_tarifas WHERE activo ='si' order by nombre");
                                                      $$result = mysql_query("SELECT tipo FROM seguro_modelos where id = $modelId");
-                                                    $modelType =  mysql_fetch_object($result);
-                                                  var_dump($modelType->tipo);    
+                                                    $modelType =  mysql_fetch_array($result);
+                                                  var_dump($modelType['tipo']);    
                                                      while ($eq = mysql_fetch_array($rescat)) { 
                                                                 
                                                                 $nombre = ucfirst(strtolower($eq['nombre'])); 
