acbf07b54b85c9d7e52acd02053404823dc125bf
Dariel Abreu <odalisdabrue@gmail.com>
2022-02-14 21:31:17 -0400

    V. 1.1.16

--- Admin/Sist.Sucursal/Seguro/Vehiculos/AJAX/Modelos.php
+++ Admin/Sist.Sucursal/Seguro/Vehiculos/AJAX/Modelos.php
@@ -6,42 +6,27 @@
 	 
 	
 		$marca_id 	= $_GET['marca_id'];
-			if($_GET['tipo']){
+		if($_GET['tipo']){
 				$tipo = $_GET['tipo'];
-
+				$tipo = $tipo + 100;
 				echo' <select name="modelo" id="modelo" style="display:compact" class="form-control">
-			<option value="0">- Seleccionar - </option>';
-		
-			$rescat = mysql_query("SELECT DISTINCT descripcion, ID, IDMARCA, tipo FROM seguro_modelos WHERE IDMARCA = '$marca_id' order by ID");
-			while ($cat = mysql_fetch_array($rescat)) {
-				if($cat['tipo']){
-					
-					if(substr_count($cat['tipo'],"".$tipo."-")>0){
-						$c = $cat['descripcion'];
-						$c_id = $cat['ID'];
-						if($_GET['selec'] == $c_id){
-						echo "<option value=\"$c_id\" selected>$c</option>"; 
+						<option value="0">- Seleccionar - </option>';
+
+				$rescat = mysql_query("SELECT DISTINCT descripcion, ID, IDMARCA, tipo FROM seguro_modelos WHERE IDMARCA = '$marca_id' order by ID");
+				
+				while ($cat = mysql_fetch_array($rescat)) {
+					if($cat['tipo']){
+						
+						if(substr_count($cat['tipo'],"".$tipo."-")>0){
+							$c = $cat['descripcion'];
+							$c_id = $cat['ID'];
+							if($_GET['selec'] == $c_id){
+							echo "<option value=\"$c_id\" selected>$c</option>"; 
+							}else{
+							echo "<option value=\"$c_id\" >$c</option>"; }	
 						}else{
-						echo "<option value=\"$c_id\" >$c</option>"; }	
-					}else{
-						echo "<option value=\"$c_id\" >$c</option>"; }						
-					}else{
-					$c = $cat['descripcion'];
-					$c_id = $cat['ID'];
-					if($_GET['selec'] == $c_id){
-					echo "<option value=\"$c_id\" selected>$c</option>"; 
-					}else{
-					echo "<option value=\"$c_id\" >$c</option>"; }
-				    }
+							echo "<option value=\"$c_id\" >$c</option>"; }						
 					
-					/*if($cat['tipo'] == $tipo){
-						$c = $cat['descripcion'];
-						$c_id = $cat['ID'];
-						if($_GET['selec'] == $c_id){
-						echo "<option value=\"$c_id\" selected>$c</option>"; 
-						}else{
-						echo "<option value=\"$c_id\" >$c</option>"; }						
-					}
 					}else{
 						$c = $cat['descripcion'];
 						$c_id = $cat['ID'];
@@ -49,8 +34,8 @@
 						echo "<option value=\"$c_id\" selected>$c</option>"; 
 						}else{
 						echo "<option value=\"$c_id\" >$c</option>"; }
-					}*/
-
+					}
+					
 				}
 
 			echo '</select>';		
